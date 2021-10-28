2df006084af36380899e1a8f5ac3c47aa10ff34c
salrey <tekdehn@gmail.com>
2021-10-28 08:30:35 -0400

    Add ContactList as a child component

---
+++ src/ContactList.js
@@ -0,0 +1,16 @@
+const ContactList = () => {
+    return (
+      <>
+        <p>Contacts</p>
+        <ul>
+          <li>Andrew Clark, 36</li>
+          <li>Brian Vaughn, 20</li>
+          <li>Dan Abramov, 40</li>
+          <li>Flarnie Marchan, 55</li>
+        </ul>
+      </>
+    )
+  }
+  
+  export default ContactList
+  
